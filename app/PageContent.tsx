9e8e13478bbb501327e7cf16a3f81fc5153cffe8
safiya nasir <lci2023053@iiitl.ac.in>
2025-01-16 18:17:05 +0530

    Issue is solved

--- app/PageContent.tsx
+++ app/PageContent.tsx
@@ -1,23 +1,50 @@
 import { unstable_cacheLife } from 'next/cache';
+import { JSX } from 'react';
 import { setTimeout } from 'timers/promises';
 
 interface Props {
   type: 'static' | 'dynamic';
 }
 
+let lastFetchedTime = Date.now();
+let lastData: JSX.Element | null = null;
+
 export async function PageContent({ type }: Props) {
   'use cache';
   unstable_cacheLife({ expire: 300, stale: 300, revalidate: 5 });
 
+  
+  if (lastData && (Date.now() - lastFetchedTime < 300000)) {
+    console.log(`${new Date().toISOString()} [${type}] Returning stale data.`);
+    return lastData;
+  }
+
   console.log(`${new Date().toISOString()} [${type}] Fetching data...`);
 
-  await setTimeout(2000); // Simulate a slow operation
+  await setTimeout(2000);
+
+  let data: JSX.Element;
+
+
+  if (type === 'static') {
+    data = (
+      <div>
+        <p>Content: Static</p>
+      </div>
+    );
+  } else {
+    data = (
+      <div>
+        <p>Content: Dynamic</p>
+      </div>
+    );
+  }
 
   console.log(`${new Date().toISOString()} [${type}] Data fetched.`);
 
-  return (
-    <div>
-      <p>Content: {type}</p>
-    </div>
-  );
+ 
+  lastFetchedTime = Date.now();
+  lastData = data;
+
+  return data;
 }
--- app/dynamic/page.tsx
+++ app/dynamic/page.tsx
@@ -1,10 +1,7 @@
-import { headers } from 'next/headers';
 import Link from 'next/link';
 import { PageContent } from '../PageContent';
 
 async function DynamicPage() {
-  await headers(); // Read headers to make this route dynamic
-
   return (
     <>
       <Link href="/">Return</Link>
